5aff792519d757c82e45ff82e063ce9dcd285ade
noxious-loris <119882804+noxious-loris@users.noreply.github.com>
2022-12-22 21:38:19 +0530

    Add files via upload

---
+++ calc 1.py
@@ -0,0 +1,34 @@
+import math
+from this import d
+a=int(input("wht is first number:"))
+b=int(input("wht is second number:"))
+c=input("wht is function:")
+def add(a,b):
+    a+b==d
+    print(d)
+def sub(a,b):
+    if a>b:
+        a-b==d
+        print(d)
+    if a<b:
+        b-a==d
+        print(d)
+def mul(a,b):
+    a*b==d
+    print(d)
+def div(a,b):
+    a/b==d
+    print(d)
+if c== "addition":
+    add(a,b)
+if c== "subtraction":
+    sub(a,b)
+if c== "multiplication":
+    mul(a,b)
+if c== "division":
+    div(a,b)
+import turtle
+bgcolor="red"
+
+
+    
